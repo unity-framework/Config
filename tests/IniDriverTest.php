b2cfb0b483e5e143a8c1d5e0c9ab5729e6e95847
e200 <eleandro@inbox.ru>
2017-08-07 22:56:50 +0100

    Added test for IniDriver

---
+++ tests/IniDriverTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Unity\Component\Config\Drivers\File\IniDriver;
+
+class IniDriverTest extends TestCase
+{
+    /**
+     * @covers IniDriver::getExt()
+     * @covers IniDriver::hasExt()
+     *
+     * `hasExt()`should return true
+     * `getExt()` should return the default extension: `ini`
+     */
+    function testPhpIsDefaultExt()
+    {
+        $driver = new IniDriver;
+
+        $this->assertTrue($driver->hasExt());
+
+        /** ini is the default extension */
+        $this->assertEquals('ini', $driver->getExt());
+    }
+
+    /**
+     * @covers IniDriver::get()
+     *
+     * Should return the value of database.user = 'root'
+     */
+    function testGet()
+    {
+        $drive = new IniDriver;
+        $source = $this->getSourceForTest();
+
+        $this->assertEquals('root', $drive->get('database.user', $source));
+    }
+
+    /**
+     * Sources must end with a slash "/"
+     *
+     * @return string
+     */
+    private function getSourceForTest()
+    {
+        return __DIR__ . '/ini/';
+    }
+}

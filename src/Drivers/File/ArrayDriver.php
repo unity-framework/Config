bf53850d3cfd953d4038a65784df9af55bbdaaed
e200 <eleandro@inbox.ru>
2017-08-07 21:56:44 +0100

    Implemented FileDriverInterface Added resolve() function

--- src/Drivers/File/ArrayDriver.php
+++ src/Drivers/File/ArrayDriver.php
@@ -2,71 +2,25 @@
 
 namespace Unity\Component\Config\Drivers\File;
 
-use Unity\Component\Config\Drivers\File\Exceptions\ConfigFileNotFoundException;
-
 class ArrayDriver extends FileDriver
 {
     function __construct()
     {
-        parent::__construct('php');
+        $this->setExt('php');
     }
 
     /**
-     * Gets the configuration
+     * Resolves and returns the array
+     * containing configurations
      *
-     * @param mixed $config The required configuration
-     * @param $sources
-     * @return mixed
-     */
-    function get($config, $sources)
-    {
-        $this->denote($config, $arrayFile, $arrayKeys);
-
-        $configArray = $this->getConfigArray($arrayFile, $sources);
-
-        return $this->getConfig($configArray, $arrayKeys);
-    }
-
-    /**
-     * Gets the configuration array containing
-     * in the array file
+     * @param $arrayFile string File
+     * containing an array with configurations
      *
-     * @param $arrayFile
-     * @param $sources
-     * @return mixed
-     * @throws ConfigFileNotFoundException
+     * @return array Array with configurations
      */
-    function getConfigArray($arrayFile, $sources)
+    function resolve($arrayFile)
     {
-        if(is_array($sources))
-            foreach ($sources as $source) {
-                $configArray = $this->requireArrayFile($arrayFile, $source);
-
-                if($configArray)
-                    return $configArray;
-            }
-
-        $configArray = $this->requireArrayFile($arrayFile, $sources);
-
-        if($configArray)
-            return $configArray;
-
-        throw new ConfigFileNotFoundException("Cannot find configuration file \"{$this->getFileNameWithExt($arrayFile)}\" in any specified sources");
-    }
-
-    /**
-     * Requires the array containing in the array file
-     *
-     * @param $arrayFile
-     * @param $source
-     * @return mixed
-     * @throws ConfigFileNotFoundException
-     */
-    function requireArrayFile($arrayFile, $source)
-    {
-        $configFile = $this->getFullPath($arrayFile, $source);
-
-        if($this->fileExists($configFile))
-            return require $configFile;
+        if ($this->fileExists($arrayFile))
+            return require $arrayFile;
     }
 }

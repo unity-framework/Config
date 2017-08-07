a0455b8da89c4b3c2418039b93f2eb1bb41a49ff
e200 <eleandro@inbox.ru>
2017-08-07 17:50:01 +0100

    Added more comments to Driver class

--- src/Drivers/Driver.php
+++ src/Drivers/Driver.php
@@ -7,7 +7,7 @@ use Unity\Component\Config\Drivers\File\Exceptions\InvalidConfigStringException;
 abstract class Driver implements DriverInterface
 {
     /**
-     * Denotes the config string dot notation
+     * Denotes the config string using dot notation
      *
      * @param $config
      * @param $root
@@ -22,6 +22,10 @@ abstract class Driver implements DriverInterface
 
         $root = $exp[0];
 
+        /**
+         * Unsetting the first element,
+         * we keep only the keys
+         */
         unset($exp[0]);
 
         foreach ($exp as $param)
@@ -36,6 +40,9 @@ abstract class Driver implements DriverInterface
      *
      * Example: database.user
      *
+     * Where `database` is the root entry
+     * and `user` is the key
+     *
      * @param $notation
      * @throws InvalidConfigStringException
      */
@@ -49,10 +56,34 @@ abstract class Driver implements DriverInterface
             );
     }
 
+    /**
+     * Do the job of get the configuration
+     * value based on `$searchKeys`
+     *
+     * @param $configArray array Array containing
+     * the configuration value
+     *
+     * @param $searchKeys array Array containing the
+     * keys to access the configuration value
+     *
+     * @return null|mixed
+     */
     function getConfig($configArray, $searchKeys)
     {
         $config = null;
 
+        /**
+         * If `$searchKeys` contains only
+         * one key, we just return the
+         * `$configArray` value associated to that key
+         *
+         * If `$searchKeys` contains more then
+         * one key, we access and store the first
+         * `$configArray` value (it must be an array)
+         * to the first `$searchKey`, and do the same
+         * with the rest of keys until they finish,
+         * the last `$searchKey` contains our value
+         */
         for($i = 0; $i < count($searchKeys); $i++)
         {
             if($i == 0)
@@ -60,6 +91,7 @@ abstract class Driver implements DriverInterface
             else
                 $config = $config[$searchKeys[$i]];
         }
+
         return $config;
     }
 }

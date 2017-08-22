533cd22bcd0f77660d8b03a2340a2852c69aa3f4
e200 <eleandro@inbox.ru>
2017-08-22 21:23:55 +0100

    Added NotationBag class

---
+++ src/Notation/NotationBag.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Unity\Component\Config\Notation;
+
+class NotationBag implements INotationBag
+{
+    /** @var $root string */
+    protected $root;
+
+    /** @var $keys array */
+    protected $keys;
+
+    /**
+     * NotationBag constructor.
+     *
+     * @param $root
+     * @param array $keys
+     */
+    function __construct($root, array $keys)
+    {
+        $this->root = $root;
+        $this->keys = $keys;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRoot()
+    {
+        return $this->root;
+    }
+
+    /**
+     * @return array
+     */
+    public function getKeys()
+    {
+        return $this->keys;
+    }
+}

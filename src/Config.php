99a5b48973c9686dcee69f959da99ad7f06685ec
e200 <eleandro@inbox.ru>
2017-09-08 23:21:41 +0100

    Removed ConfigInterface implmentation Added methods hasExt(), hasDriver(), getDriver(), getExt(), getDriversRegistry(), has()...

--- src/Config.php
+++ src/Config.php
@@ -2,21 +2,136 @@
 
 namespace Unity\Component\Config;
 
-use Unity\Component\Config\Drivers\DriverInterface;
+use Closure;
+use Unity\Component\Config\Contracts\IDriver;
+use Unity\Component\Config\Drivers\DriverFactory;
+use Unity\Component\Config\Notation\DotNotation;
+use Unity\Component\Config\Resolvers\SourceResolver;
 
-class Config implements ConfigInterface
+class Config
 {
+    protected $src;
+    protected $ext;
     protected $driver;
-    protected $source;
+    protected $sourceResolver;
 
-    function __construct(DriverInterface $driver, $source)
+    /**
+     * Config constructor.
+     *
+     * @param $src mixed Configuration source
+     * @param string|null $ext Configuration file extension, for file sources
+     * @param string|null $driver The alias to the driver to be used
+     */
+    public function __construct($src, $ext = null, $driver = null)
     {
+        $this->src = $src;
+        $this->ext = $ext;
         $this->driver = $driver;
-        $this->source = $source;
     }
 
+    /**
+     * Returns a DriversRegistry instance
+     *
+     * @return DriversRegistry
+     */
+    function getDriversRegistry()
+    {
+        return new DriversRegistry;
+    }
+
+    /**
+     * Checks ifs a configuration exists
+     *
+     * @param $config
+     */
+    function has($config)
+    {
+
+    }
+
+    /**
+     * Gets a configuration value
+     *
+     * @param $config
+     *
+     * @return mixed
+     */
     function get($config)
     {
-        return $this->driver->get($config, $this->source);
+        $notation = DotNotation::denote($config);
+
+        $src    = $this->getSource();
+        $root = $notation->getRoot();
+        $keys = $notation->getKeys();
+        $ext       = $this->getExt();
+        $driverAlias = $this->getDriverAlias();
+
+        $driversRegistry = $this->getDriversRegistry();
+
+        $src = (new SourceResolver($driversRegistry))->resolve(
+            $src,
+            $root,
+            $ext,
+            $driverAlias
+        );
+
+        $driverFactory = new DriverFactory($driversRegistry);
+
+        /*
+         * If the ConfigBuilder class don't explicitly the
+         * driver to be used, we must try auto locate a driver
+         * based on the `$src` NOR `$root` arguments
+         */
+        if($this->hasDriverAlias())
+            $driver = $driverFactory->makeFromAlias($driverAlias);
+        elseif($this->hasExt())
+            $driver = $driverFactory->makeFromExt($ext);
+        else
+            $driver = $driverFactory->makeFromFile($src);
+
+        $driver->setSource($src);
+
+        return $driver->get($keys);
+    }
+
+    /**
+     * @return bool
+     */
+    function hasDriverAlias()
+    {
+        return !is_null($this->driver);
+    }
+
+    /**
+     * @return IDriver|null
+     */
+    function getDriverAlias()
+    {
+        return $this->driver;
+    }
+
+    /**
+     * Checks if there's an extension
+     * @return bool
+     */
+    function hasExt()
+    {
+        return !is_null($this->ext);
+    }
+
+    /**
+     * @return string|null
+     */
+    function getExt()
+    {
+        return $this->ext;
+    }
+
+    /**
+     * @return mixed
+     */
+    function getSource()
+    {
+        return $this->src;
     }
 }

bb46542871b51bb117be6ddaea523075797b57ee
e200 <eleandro@inbox.ru>
2017-09-08 23:25:15 +0100

    Removed SourceResolver and DriverFactory instantiation responsability

--- src/ConfigBuilder.php
+++ src/ConfigBuilder.php
@@ -2,105 +2,73 @@
 
 namespace Unity\Component\Config;
 
-use Unity\Component\Config\Drivers\DriverInterface;
-use Unity\Component\Config\Drivers\File\ArrayDriver;
-use Unity\Component\Config\Exceptions\RequiredSourceException;
-
 class ConfigBuilder
 {
-    /**
-     * By default, ArrayDriver is the default driver
-     *
-     * @var string
-     */
-    protected $driver = ArrayDriver::class;
-
-    /** @var $source string Configurations source(s) */
-    protected $source;
+    protected $src;
+    protected $ext;
+    protected $driverAlias;
 
     /**
-     * Sets where to search for configurations
+     * Sets the source for configurations
      *
-     * The `$source` value depends on the driver
-     * you're using, for drivers that extends from
-     * the `FileDriver` abstract class, the `$source`
-     * is the path where your configuration files are
-     *
-     * @param $source string|array
-     * @return $this
-     */
-    function setSource($source)
-    {
-        $this->source = $source;
-
-        return $this;
-    }
-
-    /**
-     * Adds a source to a collection of sources.
-     *
-     * Use this function if you have more then
-     * one source for your configurations.
-     *
-     * @param $source
-     */
-    function addSource($source)
-    {
-        $this->source[] = $source;
-    }
-
-    /**
-     * Gets the source of configurations
+     * @param $src
      *
      * @return mixed
      */
-    function getSource()
+    function setSource($src)
     {
-        return $this->source;
-    }
-
-    /**
-     * Selects the driver to be used
-     *
-     * @param $driver string|object
-     * @return $this
-     */
-    function setDriver($driver)
-    {
-        $this->driver = $driver;
+        $this->src = $src;
 
         return $this;
     }
 
     /**
-     * Instantiates the selected driver
+     * Sets the extension of the configuration files
      *
-     * @return string|object
+     * @param mixed $ext
+     *
+     * @return ConfigBuilder
      */
-    function makeDriver()
+    function setExt($ext)
     {
-        /**
-         * If the user passed a DriverInterface object
-         * we just return it, otherwise we instantiate
-         * the driver selected by the user.
-         */
-        if(class_implements(DriverInterface::class))
-            return $this->driver;
+        $this->ext = $ext;
 
-        return new $this->driver;
+        return $this;
     }
 
     /**
-     * Builds the configuration instance
+     * Sets the Driver to be used to get Configuration values
+     *
+     * @param string $driver
+     *
+     * @return ConfigBuilder
+     */
+    function setDriver($driver)
+    {
+        $this->driverAlias = $driver;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    function hasDriver()
+    {
+        return !is_null($this->driverAlias);
+    }
+
+    /**
+     * Builds and returns a new instance of Config class
      *
      * @return Config
-     * @throws RequiredSourceException
      */
     function build()
     {
-        if(is_null($this->getSource()))
-            throw new RequiredSourceException("You must specify a source before you build a Config class instance.");
-
-        return new Config($this->makeDriver(), $this->getSource());
+        return new Config(
+            $this->src,
+            $this->ext,
+            $this->driverAlias
+        );
     }
 }

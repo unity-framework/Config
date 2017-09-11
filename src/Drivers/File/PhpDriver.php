92f9aaeefb47fe47a1c1a1f53ca6f3c61c320ae3
Eleandro Duzentos <eleandro@inbox.ru>
2017-09-11 17:26:36 +0000

    Apply fixes from StyleCI

--- src/Drivers/File/PhpDriver.php
+++ src/Drivers/File/PhpDriver.php
@@ -2,21 +2,22 @@
 
 namespace Unity\Component\Config\Drivers\File;
 
-use Unity\Support\File;
 use Unity\Component\Config\Drivers\FileDriver;
+use Unity\Support\File;
 
 class PhpDriver extends FileDriver
 {
     /**
-     * Returns the configuration as an array
+     * Returns the configuration as an array.
      *
      * @param $phpfile
      *
      * @return array
      */
-    function parse($phpfile)
+    public function parse($phpfile)
     {
-        if(File::exists($phpfile))
+        if (File::exists($phpfile)) {
             return require $phpfile;
+        }
     }
 }

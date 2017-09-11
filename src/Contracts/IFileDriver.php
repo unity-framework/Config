8f398d358225e78e1f7a2769e92b627cb0039975
e200 <eleandro@inbox.ru>
2017-09-11 22:38:18 +0100

    Added function parse($file)

--- src/Contracts/IFileDriver.php
+++ src/Contracts/IFileDriver.php
@@ -4,4 +4,12 @@ namespace Unity\Component\Config\Contracts;
 
 interface IFileDriver extends IDriver
 {
+    /**
+     * Parses $file content and returns its data
+     *
+     * @param $file
+     *
+     * @return array
+     */
+    function parse($file) : array;
 }

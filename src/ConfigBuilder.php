bf807bd5cda410f71ede4248be064afb93f417f8
e200 <eleandro@inbox.ru>
2017-09-16 17:57:34 +0100

    Decoupled ConfigBuilder::class from Config::class

--- src/ConfigBuilder.php
+++ src/ConfigBuilder.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace Unity\Component\Config;
+use Unity\Component\Config\SourcesMatcher;
 
 class ConfigBuilder
 {
     protected $src;
     protected $ext;
     protected $driverAlias;
+    protected $container;
+    protected $cachePath;
 
     /**
      * Sets the source for configurations
@@ -59,16 +62,113 @@ class ConfigBuilder
     }
 
     /**
+    * Sets the DI container
+    *
+    * @return ConfigBuilder
+    */
+    function setContainer(ContainerInterface $container)
+    {
+        $this->container = $container;
+
+        return $this;
+    }
+
+    /**
+    * Gets the DI container
+    *
+    * @return ContainerInterface
+    */
+    function getContainer()
+    {
+        return $this->container;
+    }
+
+    /**
+    * Checks if a ContainerInterface instance was set
+    *
+    * @return bool
+    */
+    function hasContainer()
+    {
+        return !is_null($this->container);
+    }
+
+    /**
+     * Sets the cache path
+     *
+     * It's also actives the caching
+     *
+     * @return ConfigBuilder
+     */
+    function setCachePath($path)
+    {
+        $this->cachePath = $path;
+
+        return $this;
+    }
+
+    /**
+     * Returns the cache path.
+     */
+    function getCachePath()
+    {
+        return $this->cachePath;
+    }
+
+    /**
+     * Checks if cache is enabled
+     *
+     * @return bool
+     */
+    function isCacheEnabled()
+    {
+        return !is_null($this->cachePath);
+    }
+
+    function setupDependencies()
+    {
+        $container = $this->getContainer();
+
+        $container->register('config',            Config::class);
+        $container->register('loader',            Loader::class);
+        $container->register('drivers',           DriversRegistry::class);
+        $container->register('source',            Source::class)->give($container);
+        $container->register('sourcesCollection', SourcesCollection::class);
+        $container->register('sourcesMatcher',    SourcesMatcher::class)->give($container);
+
+        if($this->isCacheEnabled()) {
+            $container->register('configCache', Cache::class)
+                ->give($this->getCachePath());
+        }
+
+        $container->register('php',  PhpDriver::class);
+        $container->register('ini',  IniDriver::class);
+        $container->register('json', JsonDriver::class);
+        $container->register('yml',  YmlDriver::class);
+    }
+
+    /**
      * Builds and returns a new instance of Config class
      *
      * @return Config
      */
     function build()
     {
-        return new Config(
-            $this->src,
-            $this->ext,
-            $this->driverAlias
-        );
+        if (!$this->hasContainer()) {
+            $this->setContainer(new Container());
+        }
+
+        $this->setupDependencies();
+
+        $container = $this->getContainer();
+
+        $data = $container->get('loader')
+            ->load(
+                $this->src,
+                $this->ext,
+                $this->driverAlias
+            );
+
+        return $container->make('config', $data);
     }
 }

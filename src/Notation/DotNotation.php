3838e3dfd38519c6680220e44e415a592cbdf767
e200 <eleandro@inbox.ru>
2017-09-16 19:01:49 +0100

    Dot notation now return only an array, no more INotationBag instances

--- src/Notation/DotNotation.php
+++ src/Notation/DotNotation.php
@@ -4,30 +4,22 @@ namespace Unity\Component\Config\Notation;
 
 use Unity\Component\Config\Contracts\INotation;
 
+/**
+ * Class DotNotation.
+ *
+ * @author Eleandro Duzentos <eleandro@inbox.ru>
+ */
 class DotNotation implements INotation
 {
     /**
      * Denotes a string using dot (.) as separator
      *
      * @param $notation
-     * @return NotationBag
+     *
+     * @return string[]
      */
     static function denote($notation)
     {
-        $exp = explode('.', $notation);
-
-        $root = $exp[0];
-        $keys = [];
-
-        /**
-         * Unsetting the root element,
-         * we keep only the keys
-         */
-        unset($exp[0]);
-
-        foreach ($exp as $key)
-            $keys[] = $key;
-
-        return new NotationBag($root, $keys);
+        return explode('.', $notation);
     }
 }

c0f4815870de8b5e5f99d5c978c3e9e96cd1fcaf
e200 <eleandro@inbox.ru>
2017-09-16 18:58:37 +0100

    Added ArrayAccess and Countable interfaces

--- src/Config.php
+++ src/Config.php
@@ -2,41 +2,30 @@
 
 namespace Unity\Component\Config;
 
+use ArrayAccess;
+use Countable;
 use Unity\Support\Arr;
 use Unity\Component\Config\Contracts\IConfig;
-use Unity\Component\Config\Contracts\INotation;
-use Unity\Component\Config\Drivers\DriverFactory;
 use Unity\Component\Config\Exceptions\ConfigNotFoundException;
-use Unity\Component\Config\Notation\NotationBag;
 use Unity\Component\Config\Notation\DotNotation;
-use Unity\Component\Config\Matcher\SourceMatcher;
-use Unity\Component\Config\DriversRegistry as Drivers;
 
 /**
- * Class Config
+ * Class Config.
+ *
+ * Gets and checks configurations data.
  *
  * @author Eleandro Duzentos <eleandro@inbox.ru>
  */
-class Config implements IConfig
+class Config implements IConfig, ArrayAccess, Countable
 {
-    protected $src;
-    protected $ext;
-    protected $driver;
-    protected $sourceResolver;
-    protected $cache = [];
+    protected $data;
 
     /**
-     * Config constructor.
-     *
-     * @param $src mixed Configuration source
-     * @param string|null $ext Configuration file extension, for file sources
-     * @param string|null $driver The alias to the driver to be used
+     * @param $data array Contains configurations data
      */
-    function __construct($src, $ext = null, $driver = null)
+    function __construct(array $data)
     {
-        $this->src    = $src;
-        $this->ext    = $ext;
-        $this->driver = $driver;
+        $this->data = $data;
     }
 
     /**
@@ -48,25 +37,9 @@ class Config implements IConfig
      */
     function get($config)
     {
-        $notation = DotNotation::denote($config);
+        $keys = DotNotation::denote($config);
 
-        $root = $notation->getRoot();
-
-        if ($this->isCached($root)) {
-            $data = $this->getCache($root);
-        } else {
-            $data = $this->getData($root);
-
-            $this->setCache($root, $data);
-        }
-
-        if ($notation->hasKeys()) {
-            $keys = $notation->getKeys();
-
-            return Arr::nestedGet($keys, $data);
-        } else {
-            return $data[$root];
-        }
+        return Arr::nestedGet($this->data, $keys);
     }
 
     /**
@@ -78,125 +51,94 @@ class Config implements IConfig
      */
     function has($config)
     {
-        $notation = DotNotation::denote($config);
+        $keys = DotNotation::denote($config);
 
-        $root = $notation->getRoot();
-        $keys = $notation->getKeys();
-
-        if ($this->isCached($root)) {
-            $data = $this->getCache($root);
-        } else {
-            $data = $this->getData($root);
-
-            $this->setCache($root, $data);
-        }
-
-        return Arr::nestedHas($keys, $data);
-    }
-
-    function isCached($config)
-    {
-        return isset($this->cache[$config]);
-    }
-
-    function setCache($key, $data)
-    {
-        $this->cache[$key] = $data;
-    }
-
-    function getCache($key)
-    {
-        return $this->cache[$key];
-    }
-
-    function getData($root)
-    {
-        $source      = $this->getSource();
-        $ext         = $this->getExt();
-        $driverAlias = $this->getDriverAlias();
-
-        $drivers = $this->getDrivers();
-
-        $src = $this->getSourceMatcher($drivers)->match(
-                $source,
-                $root,
-                $ext,
-                $driverAlias
-            );
-
-        if($src->isEmpty())
-            throw new ConfigNotFoundException("Cannot find the requested configuration");
-
-        $driverFactory = $this->getDriverFactory($drivers);
-
-        if($this->hasDriverAlias())
-            $driver = $driverFactory->makeFromAlias($driverAlias);
-        elseif($this->hasExt())
-            $driver = $driverFactory->makeFromExt($ext);
-        elseif($src->isFile())
-            $driver = $driverFactory->makeFromFile($src->get());
-
-        return $driver->load($src->get());
+        return Arr::nestedHas($this->data, $keys);
     }
 
     /**
-     * Returns a DriversRegistry instance
+     * Gets all available configurations
      *
-     * @return DriversRegistry
+     * @return array
      */
-    function getDrivers()
+    function getAll()
     {
-        return new Drivers;
-    }
-
-    function getSourceMatcher($drivers)
-    {
-        return new SourceMatcher($drivers);
-    }
-
-    function getDriverFactory($drivers)
-    {
-        return new DriverFactory($drivers);
+        return $this->data;
     }
 
     /**
-     * @return bool
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
      */
-    function hasDriverAlias()
+    public function offsetExists($offset)
     {
-        return !is_null($this->driver);
+        // TODO: Implement offsetExists() method.
     }
 
     /**
-     * @return string|null
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     * @return mixed Can return all value types.
+     * @since 5.0.0
      */
-    function getDriverAlias()
+    public function offsetGet($offset)
     {
-        return $this->driver;
+        // TODO: Implement offsetGet() method.
     }
 
     /**
-     * Checks if there's an extension
-     * @return bool
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     * @return void
+     * @since 5.0.0
      */
-    function hasExt()
+    public function offsetSet($offset, $value)
     {
-        return !is_null($this->ext);
+        // TODO: Implement offsetSet() method.
     }
 
     /**
-     * @return string|null
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     * @return void
+     * @since 5.0.0
      */
-    function getExt()
+    public function offsetUnset($offset)
     {
-        return $this->ext;
+        // TODO: Implement offsetUnset() method.
     }
 
     /**
-     * @return mixed
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     * @since 5.1.0
      */
-    function getSource()
+    public function count()
     {
-        return $this->src;
+        return count($this->data);
     }
 }

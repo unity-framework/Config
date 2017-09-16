f8212e3de63363bfff72a55b2d13313e312bdcfe
e200 <eleandro@inbox.ru>
2017-09-16 19:01:07 +0100

    FileDriver adapter to Driver abstract class

--- src/Drivers/FileDriver.php
+++ src/Drivers/FileDriver.php
@@ -2,6 +2,28 @@
 
 namespace Unity\Component\Config\Drivers;
 
-abstract class FileDriver extends Driver
+use Unity\Component\Config\Contracts\IFileDriver;
+
+/**
+ * class FileDriver.
+ *
+ * Abstract class that implements IFileDriver interface.
+ *
+ * Implements IFileDriver interface.
+ *
+ * @author Eleandro Duzentos <eleandro@inbox.ru|github:e200>
+ */
+abstract class FileDriver extends Driver implements IFileDriver
 {
+    /**
+     * Returns all configurations data
+     *
+     * @param mixed $file string The file containing the configurations
+     *
+     * @return array
+     */
+    function load($file) : array
+    {
+        return $this->parse($file);
+    }
 }

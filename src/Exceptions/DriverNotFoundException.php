cf5aa7e021d3a04e62da8f2d2d5151ea873a0c2e
e200 <eleandro@inbox.ru>
2017-10-24 23:48:04 +0100

    Added new exceptions

---
+++ src/Exceptions/DriverNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Unity\Component\Config\Exceptions;
+
+use Exception;
+use Throwable;
+
+class DriverNotFoundException extends Exception
+{
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
---
+++ src/Exceptions/UnreadableFolderException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Unity\Component\Config\Exceptions;
+
+use Exception;
+use Throwable;
+
+class UnreadableFolderException extends Exception
+{
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}

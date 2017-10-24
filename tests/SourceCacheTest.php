f45c5aa3f6a4e547fb4379eab2076316f42d8771
e200 <eleandro@inbox.ru>
2017-10-24 23:46:47 +0100

    Added SourceCacheTest

---
+++ tests/SourceCacheTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    protected $virtualFolder;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $dir = [
+            'database.php' => "<?php return ['user' => 'root'];"
+        ];
+
+        $this->virtualFolder = vfsStream::setup(
+            'configs',
+            444,
+            $dir
+        );
+    }
+}

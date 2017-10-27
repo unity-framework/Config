c9d8fdd353d3ccdf50d29271cb80ba992ef590d5
e200 <eleandro@inbox.ru>
2017-10-27 09:36:56 +0100

    Added test for SourceFile

---
+++ tests/SourceFileTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Unity\Contracts\Config\Drivers\IDriver;
+use Unity\Component\Config\Sources\SourceFile;
+
+class SourceFileTest extends TestCase
+{
+    public function testGetKey()
+    {
+        $fileSource = $this->getFileSource(true, null);
+
+        $this->assertTrue($fileSource->getKey());
+    }
+
+    public function testGetSource()
+    {
+        $fileSource = $this->getFileSource(null, true);
+
+        $this->assertTrue($fileSource->getSource());
+    }
+
+    public function testGetData()
+    {
+        $driverMock = $this->createMock(IDriver::class);
+
+        $driverMock
+            ->expects($this->once())
+            ->method('parse')
+            ->willReturn([true]);
+
+        $fileSource = $this->getFileSource(null, null, $driverMock);
+
+        $this->assertEquals([true], $fileSource->getData());
+    }
+
+    public function getFileSource($key, $source, IDriver $driver = null)
+    {
+        if (!$driver) {
+            $driver = $this->createMock(IDriver::class);
+        }
+
+        return new SourceFile($key, $source, $driver);
+    }
+}

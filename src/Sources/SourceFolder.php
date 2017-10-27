4526ab8d452a52f033c8d6551f3d25f2270b9ec3
e200 <eleandro@inbox.ru>
2017-10-27 09:35:07 +0100

    Renamed FolderSource to SourceFolder.

---
+++ src/Sources/SourceFolder.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Unity\Component\Config\Sources;
+
+use Unity\Contracts\Config\Sources\ISourceFile;
+use Unity\Contracts\Config\Sources\ISource;
+use Unity\Contracts\Config\Sources\ISourceFilesMatcher;
+
+/**
+ * Class SourceFolder.
+ *
+ * Represents a folder source.
+ *
+ * @author Eleandro Duzentos <eleandro@inbox.ru>
+ */
+class SourceFolder implements ISource
+{
+    protected $folder;
+    protected $driver;
+    protected $ext;
+
+    protected $sourceFilesMatcher;
+
+    public function __construct(
+        $folder,
+        $driver,
+        $ext,
+        ISourceFilesMatcher $sourceFilesMatcher
+        ) {
+        $this->folder = $folder;
+        $this->driver = $driver;
+        $this->ext = $ext;
+
+        $this->sourceFilesMatcher = $sourceFilesMatcher;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSource()
+    {
+        return $this->folder;
+    }
+
+    /**
+     * Gets all available configurations data.
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        $data = [];
+
+        foreach ($this->getSourceFiles() as $sourceFile) {
+            $data[$sourceFile->getKey()] = $sourceFile->getData();
+        }
+
+        return $data;
+    }
+
+    /**
+     * Returns an array containing all source files
+     * founded on `$this->folder`.
+     *
+     * @return ISourceFile[]
+     */
+    protected function getSourceFiles()
+    {
+        return $this->sourceFilesMatcher->match(
+            $this->folder,
+            $this->driver,
+            $this->ext
+        );
+    }
+}

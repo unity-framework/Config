fb0879df0135f3dbdf52a3a724425460b076306c
e200 <eleandro@inbox.ru>
2017-10-27 09:36:30 +0100

    Added test for SourceFactory

---
+++ tests/SourceFactoryTest.php
@@ -0,0 +1,138 @@
+<?php
+
+use Unity\Support\FileInfo;
+use e200\MakeAccessible\Make;
+use PHPUnit\Framework\TestCase;
+use Unity\Contracts\Container\IContainer;
+use Unity\Contracts\Config\Sources\IFileSource;
+use Unity\Component\Config\Factories\SourceFactory;
+use Unity\Contracts\Config\Factories\IDriverFactory;
+
+class SourceFactoryTest extends TestCase
+{
+    public function testResolveDriverWithFile()
+    {
+        $driverFactoryMock = $this->createMock(IDriverFactory::class);
+        
+        /**
+         * SourceFactory will try to resolve the driver. 
+         */
+        $driverFactoryMock
+            ->expects($this->once())
+            ->method('makeFromFile')
+            ->willReturn(true);
+
+        $sourceFactory = $this->getSourceFactory($driverFactoryMock);            
+        $sourceFactory = Make::accessible($sourceFactory);
+
+        $driver = $sourceFactory->resolveDriver('folder/configs.yml', null, null);
+        
+        $this->assertTrue($driver);
+    }
+
+    public function testResolveDriverWithDriver()
+    {
+        $driverFactoryMock = $this->createMock(IDriverFactory::class);
+        
+        /**
+         * SourceFactory will try to resolve the driver 
+         */
+        $driverFactoryMock
+            ->expects($this->once())
+            ->method('makeFromAlias')
+            ->willReturn(true);
+
+        $sourceFactory = $this->getSourceFactory($driverFactoryMock);            
+        $sourceFactory = Make::accessible($sourceFactory);
+
+        $driver = $sourceFactory->resolveDriver('', 'php', null);
+        
+        $this->assertTrue($driver);
+    }
+
+    public function testResolveDriverWithExt()
+    {
+        $driverFactoryMock = $this->createMock(IDriverFactory::class);
+        
+        /**
+         * SourceFactory will try to resolve the driver 
+         */
+        $driverFactoryMock
+            ->expects($this->once())
+            ->method('makeFromExt')
+            ->willReturn(true);
+
+        $sourceFactory = $this->getSourceFactory($driverFactoryMock);            
+        $sourceFactory = Make::accessible($sourceFactory);
+
+        $driver = $sourceFactory->resolveDriver('', null, '');
+        
+        $this->assertTrue($driver);
+    }
+
+    public function testMakeFromFile()
+    {
+        $driverFactoryMock = $this->createMock(IDriverFactory::class);
+        
+        $driverFactoryMock
+            ->expects($this->once())
+            ->method('makeFromFile')
+            ->willReturn(true);
+
+        $fileInfoMock = $this->createMock(FileInfo::class);
+        
+        $fileInfoMock
+            ->expects($this->once())
+            ->method('name')
+            ->willReturn(null);
+
+        $containerMock = $this->createMock(IContainer::class);
+        
+        $containerMock
+            ->expects($this->once())
+            ->method('make')
+            ->willReturn(true);
+
+        $sourceFactory = $this->getSourceFactory($driverFactoryMock, $containerMock, $fileInfoMock);
+
+        $source = $sourceFactory->makeFromFile('folder/configs.yml');
+
+        $this->assertTrue($source);
+    }
+
+    public function testMakeFromFolder()
+    {
+        $containerMock = $this->createMock(IContainer::class);
+        
+        $containerMock
+            ->expects($this->once())
+            ->method('make')
+            ->willReturn(true);
+
+        $sourceFactory = $this->getSourceFactory(null, $containerMock);
+
+        $source = $sourceFactory->makeFromFolder('folder/');
+
+        $this->assertTrue($source);
+    }
+
+    public function getSourceFactory(
+        IDriverFactory $driverFactory = null,
+        IContainer $container = null,
+        FileInfo $fileInfo = null
+        ) {
+        if (!$driverFactory) {
+            $driverFactory = $this->createMock(IDriverFactory::class);
+        }
+
+        if (!$container) {
+            $container = $this->createMock(IContainer::class);
+        }
+
+        if (!$fileInfo) {
+            $fileInfo = $this->createMock(FileInfo::class);
+        }
+
+        return new SourceFactory($driverFactory, $container, $fileInfo);
+    }
+}

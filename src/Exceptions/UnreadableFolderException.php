57ff71c56d232f134e5c73ab3fa85c168c3c62ba
e200 <eleandro@inbox.ru>
2017-10-29 23:43:49 +0100

    Added author @link

--- src/Exceptions/UnreadableFolderException.php
+++ src/Exceptions/UnreadableFolderException.php
@@ -5,6 +5,12 @@ namespace Unity\Component\Config\Exceptions;
 use Exception;
 use Throwable;
 
+/**
+ * Class UnreadableFolderException.
+ *
+ * @author Eleandro Duzentos <eleandro@inbox.ru>
+ * @link   https://github.com/e200/
+ */
 class UnreadableFolderException extends Exception
 {
     public function __construct($message = '', $code = 0, Throwable $previous = null)
--- src/Exceptions/UnsupportedExtensionException.php
+++ src/Exceptions/UnsupportedExtensionException.php
@@ -5,6 +5,12 @@ namespace Unity\Component\Config\Exceptions;
 use Exception;
 use Throwable;
 
+/**
+ * Class UnsupportedExtensionException.
+ *
+ * @author Eleandro Duzentos <eleandro@inbox.ru>
+ * @link   https://github.com/e200/
+ */
 class UnsupportedExtensionException extends Exception
 {
     public function __construct($message = '', $code = 0, Throwable $previous = null)

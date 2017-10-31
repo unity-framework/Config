ed19183d64f09302534993f930be2b64597f6d06
e200 <eleandro@inbox.ru>
2017-10-31 14:17:00 +0100

    Loader now throws UnreadableSourceException on load unreadable sources.

--- src/Loader.php
+++ src/Loader.php
@@ -5,11 +5,12 @@ namespace Unity\Component\Config;
 use Unity\Contracts\Config\ILoader;
 use Unity\Contracts\Config\Factories\ISourceFactory;
 use Unity\Component\Config\Exceptions\DriverNotFoundException;
+use Unity\Component\Config\Exceptions\UnreadableSourceException;
 
 /**
  * Class Loader.
  *
- * Loads all available and supported configuration sources.
+ * Loads all available and supported config sources.
  *
  * @author Eleandro Duzentos <eleandro@inbox.ru>
  * @link   https://github.com/e200/
@@ -36,16 +37,18 @@ class Loader implements ILoader
      */
     public function load($source, $driver, $ext)
     {
-        $sourceInstance = null;
+        if (!is_readable($source)) {
+            throw new UnreadableSourceException('Permission denied while trying to read source.');
+        }
 
         if (is_file($source)) {
             $sourceInstance = $this->sourceFactory->makeFromFile($source, $driver, $ext);
+
+            if ($sourceInstance === false) {
+                throw new DriverNotFoundException('Cannot find any driver that supports the given source.');
+            }
         } elseif (is_dir($source)) {
             $sourceInstance = $this->sourceFactory->makeFromFolder($source, $driver, $ext);
-        }
-
-        if ($sourceInstance === false) {
-            throw new DriverNotFoundException('Cannot find any driver that supports the given source.');
         }
 
         return $sourceInstance->getData();
